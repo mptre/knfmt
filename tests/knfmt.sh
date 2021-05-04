8670d9628e5007b5c7a9bf56520c8bdbbaeb381e
Anton Lindqvist <anton@basename.se>
2021-05-04 07:54:29 +0200

    apply exec to error test cases as well

--- tests/knfmt.sh
+++ tests/knfmt.sh
@@ -12,7 +12,7 @@ commstrip() {
 case "$1" in
 error-*)
 	_err=0
-	${KNFMT} "$1" >/dev/null 2>&1 || _err="$?"
+	${EXEC:-} ${KNFMT} "$1" >/dev/null 2>&1 || _err="$?"
 	if [ "$_err" -ne 1 ]; then
 		echo "${1}; expected exit 1, got ${_err}" 1>&2
 		exit 1
