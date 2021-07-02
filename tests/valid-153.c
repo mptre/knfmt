mptre/knfmt

commit a9b68e6913617577a5078071f281f88b234ea62d

remove unused logic in doc_print()
Dead code since printing of optional lines are by now postponed.

doc.c
@@ -745,16 +745,6 @@ doc_print(const struct doc *dc, struct doc_state *st, const char *str,
 		if (st->st_nlines >= 2)
 			return;
 		st->st_nlines++;
-
-		/*
-		 * Suppress optional line(s) while emitting a line. Mixing the
-		 * two results in odd formatting.
-		 */
-		if ((flags & DOC_PRINT_FLAG_NEWLINE) == 0 && st->st_optline) {
-			doc_trace(dc, st, "%s: optline %d -> 0",
-			    __func__, st->st_optline);
-			st->st_optline = 0;
-		}
 	} else {
 		st->st_nlines = 0;
 	}

commit 837c0fcdff5c316c5a5212bc3dd53a94bd5864b5

fix optional line regression
Introduced in commit a9b68e6 ("remove unused logic in doc_print()").

Makefile
@@ -236,6 +236,7 @@ DISTFILES+=	tests/valid-150.c
 DISTFILES+=	tests/valid-150.ok
 DISTFILES+=	tests/valid-151.c
 DISTFILES+=	tests/valid-152.c
+DISTFILES+=	tests/valid-153.c
 DISTFILES+=	token.h
 
 all: ${PROG_knfmt}

doc.c
@@ -121,7 +121,7 @@ static void	__doc_trace_enter(const struct doc *, struct doc_state *);
 static void	__doc_trace_leave(const struct doc *, struct doc_state *);
 
 static char		*docstr(const struct doc *, char *, size_t);
-static const char	*indentstr(const struct doc *);
+static const char	*intstr(const struct doc *);
 static const char	*statestr(const struct doc_state *, unsigned int,
     char *, size_t);
 
@@ -565,9 +565,13 @@ doc_exec1(const struct doc *dc, struct doc_state *st)
 		break;
 
 	case DOC_OPTIONAL:
-		/* Note, could already be cleared by doc_print(). */
-		if (dc->dc_int > 0 || -dc->dc_int <= st->st_optline)
+		if (st->st_optline == DOC_OPTIONAL_STICKY) {
+			if (-dc->dc_int == DOC_OPTIONAL_STICKY)
+				st->st_optline = 0;
+		} else if (dc->dc_int > 0 || -dc->dc_int <= st->st_optline) {
+			/* Note, could already be cleared by doc_print(). */
 			st->st_optline += dc->dc_int;
+		}
 		break;
 	}
 
@@ -745,6 +749,18 @@ doc_print(const struct doc *dc, struct doc_state *st, const char *str,
 		if (st->st_nlines >= 2)
 			return;
 		st->st_nlines++;
+
+		/*
+		 * Suppress optional line(s) while emitting a line. Mixing the
+		 * two results in odd formatting.
+		 */
+		if ((flags & DOC_PRINT_FLAG_NEWLINE) == 0 &&
+		    st->st_optline > 0 &&
+		    st->st_optline != DOC_OPTIONAL_STICKY) {
+			doc_trace(dc, st, "%s: optline %d -> 0",
+			    __func__, st->st_optline);
+			st->st_optline = 0;
+		}
 	} else {
 		st->st_nlines = 0;
 	}
@@ -850,14 +866,17 @@ __doc_trace_enter(const struct doc *dc, struct doc_state *st)
 		break;
 
 	case DOC_INDENT:
-	case DOC_DEDENT: {
+	case DOC_DEDENT:
+	case DOC_OPTIONAL: {
 		const char *str;
 
 		fprintf(stderr, "(");
-		if ((str = indentstr(dc)) != NULL)
+		if ((str = intstr(dc)) != NULL)
 			fprintf(stderr, "%s", str);
 		else
 			fprintf(stderr, "%d", dc->dc_int);
+		if (dc->dc_type == DOC_OPTIONAL)
+			fprintf(stderr, ")");
 		break;
 	}
 
@@ -897,7 +916,6 @@ __doc_trace_enter(const struct doc *dc, struct doc_state *st)
 
 	case DOC_MUTE:
 	case DOC_NEWLINE:
-	case DOC_OPTIONAL:
 		fprintf(stderr, "(%d)", dc->dc_int);
 		break;
 	}
@@ -983,15 +1001,19 @@ docstr(const struct doc *dc, char *buf, size_t bufsiz)
 }
 
 static const char *
-indentstr(const struct doc *dc)
+intstr(const struct doc *dc)
 {
 	switch (dc->dc_int) {
+	case DOC_DEDENT_NONE:
+		return "NONE";
 	case DOC_INDENT_PARENS:
 		return "PARENS";
 	case DOC_INDENT_FORCE:
 		return "FORCE";
-	case DOC_DEDENT_NONE:
-		return "NONE";
+	case DOC_OPTIONAL_STICKY:
+		return "STICKY";
+	case -DOC_OPTIONAL_STICKY:
+		return "-STICKY";
 	}
 	return NULL;
 }
@@ -1000,6 +1022,7 @@ static const char *
 statestr(const struct doc_state *st, unsigned int depth, char *buf,
     size_t bufsiz)
 {
+	char optline[4];
 	unsigned char mode = 'U';
 	int n;
 
@@ -1011,9 +1034,13 @@ statestr(const struct doc_state *st, unsigned int depth, char *buf,
 		mode = 'M';
 		break;
 	}
+	if (st->st_optline == DOC_OPTIONAL_STICKY)
+		(void)snprintf(optline, sizeof(optline), "  S");
+	else
+		(void)snprintf(optline, sizeof(optline), "%3d", st->st_optline);
 	/* Keep in sync with doc_state_header(). */
-	n = snprintf(buf, bufsiz, "[D] [%c,%3u,%3u,%3d,%3d]",
-	    mode, st->st_pos, depth, st->st_mute, st->st_optline);
+	n = snprintf(buf, bufsiz, "[D] [%c,%3u,%3u,%3d,%s]",
+	    mode, st->st_pos, depth, st->st_mute, optline);
 	if (n < 0 || n >= (ssize_t)bufsiz)
 		errc(1, ENAMETOOLONG, "%s", __func__);
 	return buf;

extern.h
@@ -305,14 +305,15 @@ struct doc	*__doc_alloc(enum doc_type, struct doc *, int, const char *,
 	__doc_alloc(DOC_OPTIONAL, (b), (a), __func__, __LINE__)
 
 /*
- * Sentinels honored by doc_alloc_dedent() and doc_alloc_indent(). The numbers
- * are something arbitrary large enough to never conflict with any actual
- * indentation. Since the numbers are compared with signed integers, favor
- * integer literals over hexadecimal ones.
+ * Sentinels honored by document allocation routines. The numbers are something
+ * arbitrary large enough to never conflict with any actual value. Since the
+ * numbers are compared with signed integers, favor integer literals over
+ * hexadecimal ones.
  */
-#define DOC_DEDENT_NONE		512	/* remove all indentation */
-#define DOC_INDENT_PARENS	1024	/* entering parenthesis */
-#define DOC_INDENT_FORCE	2048	/* force indentation */
+#define DOC_DEDENT_NONE		111	/* remove all indentation */
+#define DOC_INDENT_PARENS	222	/* entering parenthesis */
+#define DOC_INDENT_FORCE	333	/* force indentation */
+#define DOC_OPTIONAL_STICKY	444	/* always allow optional line(s) */
 
 #define doc_alloc_indent(a, b) \
 	__doc_alloc_indent(DOC_INDENT, (a), (b), __func__, __LINE__)

parser.c
@@ -518,9 +518,9 @@ parser_exec_decl_braces(struct parser *pr, struct doc *dc)
 	memset(&rl, 0, sizeof(rl));
 	ruler_init(&rl);
 	concat = doc_alloc(DOC_CONCAT, dc);
-	doc_alloc_optional(1, concat);
+	doc_alloc_optional(DOC_OPTIONAL_STICKY, concat);
 	error = parser_exec_decl_braces1(pr, concat, &rl);
-	doc_alloc_optional(-1, concat);
+	doc_alloc_optional(-DOC_OPTIONAL_STICKY, concat);
 	ruler_exec(&rl);
 	ruler_free(&rl);
 	return error;

tests/Makefile
@@ -163,6 +163,7 @@ TESTS+=	valid-149.c
 TESTS+=	valid-150.c
 TESTS+=	valid-151.c
 TESTS+=	valid-152.c
+TESTS+=	valid-153.c
 
 TESTS+=	../buffer.c
 TESTS+=	../compat-pledge.c

tests/valid-153.c
@@ -1,0 +1,10 @@
+/*
+ * Optional line regression.
+ */
+
+int
+main(void)
+{
+	vcpu->vc_msr_bitmap_va = (vaddr_t)km_alloc(PAGE_SIZE, &kv_page, &kp_zero,
+	    &kd_waitok);
+}

tests/valid-153.ok
@@ -1,0 +1,6 @@
+int
+main(void)
+{
+	vcpu->vc_msr_bitmap_va = (vaddr_t)km_alloc(PAGE_SIZE, &kv_page,
+	    &kp_zero, &kd_waitok);
+}