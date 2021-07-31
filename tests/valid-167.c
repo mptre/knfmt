mptre/knfmt

commit adaa6eeaf1ffa360e3e2fa9ae5b78c6e9a96861f

recognize function pointer types without arguments

Makefile
@@ -333,6 +333,7 @@ DISTFILES+=	tests/valid-160.c
 DISTFILES+=	tests/valid-161.c
 DISTFILES+=	tests/valid-162.c
 DISTFILES+=	tests/valid-163.c
+DISTFILES+=	tests/valid-164.c
 DISTFILES+=	token.h
 DISTFILES+=	util.c
 

lexer.c
@@ -905,7 +905,7 @@ lexer_peek_if_type(struct lexer *lx, struct token **tk)
 
 			/* Consume the identifier, i.e. preprocessor macro. */
 			lexer_if(lx, TOKEN_IDENT, &t);
-		} else if (lexer_peek_if_func_ptr(lx, &t)) {
+		} else if (ntokens > 0 && lexer_peek_if_func_ptr(lx, &t)) {
 			struct token *align;
 
 			/*
@@ -1852,14 +1852,15 @@ lexer_peek_if_func_ptr(struct lexer *lx, struct token **tk)
 			lexer_if(lx, TOKEN_LITERAL, NULL);
 			lexer_if(lx, TOKEN_RSQUARE, NULL);
 		}
-		if (lexer_if(lx, TOKEN_RPAREN, NULL) &&
-		    lexer_peek_if(lx, TOKEN_LPAREN, &lparen) &&
-		    lexer_if_pair(lx, TOKEN_LPAREN, TOKEN_RPAREN, tk)) {
-			/*
-			 * Annotate the left parenthesis, used by
-			 * parser_exec_type().
-			 */
-			lparen->tk_flags |= TOKEN_FLAG_TYPE_ARGS;
+		if (lexer_if(lx, TOKEN_RPAREN, tk)) {
+			if (lexer_peek_if(lx, TOKEN_LPAREN, &lparen) &&
+			    lexer_if_pair(lx, TOKEN_LPAREN, TOKEN_RPAREN, tk)) {
+				/*
+				 * Annotate the left parenthesis, used by
+				 * parser_exec_type().
+				 */
+				lparen->tk_flags |= TOKEN_FLAG_TYPE_ARGS;
+			}
 			peek = 1;
 		}
 	}

tests/Makefile
@@ -199,6 +199,7 @@ TESTS+=	valid-160.c
 TESTS+=	valid-161.c
 TESTS+=	valid-162.c
 TESTS+=	valid-163.c
+TESTS+=	valid-164.c
 
 TESTS+=	../buffer.c
 TESTS+=	../compat-pledge.c

t.c
@@ -185,6 +185,8 @@ main(int argc, char *argv[])
 	error |= test_lexer_peek_if_type("int (*f(void))(void)", "int");
 	error |= test_lexer_peek_if_type("void (*f[1])(void)",
 	    "void ( * f [ 1 ] ) ( void )");
+	error |= test_lexer_peek_if_type("void (*)",
+	    "void ( * )");
 
 	error |= test_lexer_read("<", "LESS");
 	error |= test_lexer_read("<x", "LESS IDENT");

tests/valid-164.c
@@ -1,0 +1,9 @@
+/*
+ * Function pointer without arguments.
+ */
+
+int
+main(void)
+{
+	task_set(&crp->crp_task, (void (*))crypto_invoke, crp);
+}

commit 86fead2d278e35fc2c776d0647cf0852092125f3

do not confuse certain function calls and function pointers
Regression introduced in commit adaa6ee ("recognize function pointer
types without arguments").

Makefile
@@ -336,6 +336,7 @@ DISTFILES+=	tests/valid-163.c
 DISTFILES+=	tests/valid-164.c
 DISTFILES+=	tests/valid-165.c
 DISTFILES+=	tests/valid-166.c
+DISTFILES+=	tests/valid-167.c
 DISTFILES+=	token.h
 DISTFILES+=	util.c
 

lexer.c
@@ -1815,8 +1815,16 @@ lexer_peek_if_func_ptr(struct lexer *lx, struct token **tk)
 				 * parser_exec_type().
 				 */
 				lparen->tk_flags |= TOKEN_FLAG_TYPE_ARGS;
+
+				peek = 1;
+			} else if (!lexer_if(lx, TOKEN_SEMI, NULL)) {
+				/*
+				 * Function pointer lacking arguments wrapped in
+				 * parenthesis. Be careful here in order to not
+				 * confuse a function call statement.
+				 */
+				peek = 1;
 			}
-			peek = 1;
 		}
 	}
 	lexer_peek_leave(lx, &s);

tests/Makefile
@@ -202,6 +202,7 @@ TESTS+=	valid-163.c
 TESTS+=	valid-164.c
 TESTS+=	valid-165.c
 TESTS+=	valid-166.c
+TESTS+=	valid-167.c
 
 TESTS+=	../buffer.c
 TESTS+=	../compat-pledge.c

tests/valid-167.c
@@ -1,0 +1,9 @@
+/*
+ * Function pointer type regression in lexer_peek_if_type().
+ */
+
+int
+main(void)
+{
+	free(*buf);
+}