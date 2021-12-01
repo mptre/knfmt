adbbdd3be56d8fdef7ca55f998210b7864cf43f5
Anton Lindqvist <anton@basename.se>
2021-12-01 20:30:22 +0100

    add mkls wrapper

---
+++ mkls.sh
@@ -0,0 +1,30 @@
+set -e
+
+mkls "$@" \
+SRCS \
+	!(knfmt|t).c \
+	-- \
+KNFMT \
+	!(compat-*).c !(compat-*|config).h \
+	compat-pledge.c \
+	-- \
+DISTFILES \
+	*.c !(config).h *.md \
+	GNUmakefile LICENSE Makefile configure knfmt.1 \
+	tests/*.+(c|ok|patch|sh) \
+	tests/GNUmakefile tests/Makefile
+
+cd tests
+mkls "$@" \
+TESTS \
+	diff*.c \
+	-- \
+TESTS \
+	error*.c \
+	-- \
+TESTS \
+	valid*.c \
+	-- \
+TESTS \
+	../!(compat-*).c ../!(compat-*|config).h \
+	../compat-pledge.c
