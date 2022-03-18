mptre/knfmt

commit 018d346134601a8de84922636ec3759bc280985b

only grow the buffer if more than half of the space is utilized
Prevents from running out of memory due to too eager allocations.

buffer.c
@@ -151,6 +151,9 @@ buffer_grow(struct buffer *bf, unsigned int shift)
 {
 	size_t newsiz;
 
+	if (bf->bf_len < bf->bf_siz / 2)
+		return;
+
 	newsiz = bf->bf_siz << shift;
 	bf->bf_ptr = realloc(bf->bf_ptr, newsiz);
 	if (bf->bf_ptr == NULL)

commit f05b6c2aabbe0f9a26380da5bb08fb059d6dffe4

fix broken buffer_grow() logic
Regression introduced in commit 018d346 ("only grow the buffer if more than half
of the space is utilized").

Makefile
@@ -413,6 +413,7 @@ DISTFILES+=	tests/valid-200.c
 DISTFILES+=	tests/valid-201.c
 DISTFILES+=	tests/valid-202.c
 DISTFILES+=	tests/valid-203.c
+DISTFILES+=	tests/valid-204.c
 DISTFILES+=	token.h
 DISTFILES+=	util.c
 

buffer.c
@@ -60,7 +60,8 @@ buffer_read(const char *path)
 		if (n == 0)
 			break;
 		bf->bf_len += n;
-		buffer_grow(bf, 1);
+		if (bf->bf_len > bf->bf_siz / 2)
+			buffer_grow(bf, 1);
 	}
 
 	close(fd);
@@ -165,9 +166,6 @@ static void
 buffer_grow(struct buffer *bf, unsigned int shift)
 {
 	size_t newsiz;
-
-	if (bf->bf_len < bf->bf_siz / 2)
-		return;
 
 	newsiz = bf->bf_siz << shift;
 	bf->bf_ptr = realloc(bf->bf_ptr, newsiz);

tests/Makefile
@@ -248,6 +248,7 @@ TESTS+=	valid-200.c
 TESTS+=	valid-201.c
 TESTS+=	valid-202.c
 TESTS+=	valid-203.c
+TESTS+=	valid-204.c
 
 TESTS+=	simple-001.c
 TESTS+=	simple-002.c

tests/valid-204.c
@@ -1,0 +1,7 @@
+/*
+ * AFL
+ */
+
+#Gef									\
+		)רי							\
+		)רת				ÿ		\			 Gef	י\…		)רת							\			!(0B