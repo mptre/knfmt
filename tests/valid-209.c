mptre/knfmt

commit 33ed4b4f2cba9537a15950651965cd28080787c7

do not confuse queue(3) macros as declarations

Makefile
@@ -73,7 +73,6 @@ DISTFILES+=	t.c
 DISTFILES+=	tests/GNUmakefile
 DISTFILES+=	tests/Makefile
 DISTFILES+=	tests/bug-001.c
-DISTFILES+=	tests/bug-002.c
 DISTFILES+=	tests/bug-003.c
 DISTFILES+=	tests/diff-001.c
 DISTFILES+=	tests/diff-001.ok
@@ -408,6 +407,7 @@ DISTFILES+=	tests/valid-199.ok
 DISTFILES+=	tests/valid-200.c
 DISTFILES+=	tests/valid-201.c
 DISTFILES+=	tests/valid-202.c
+DISTFILES+=	tests/valid-203.c
 DISTFILES+=	token.h
 DISTFILES+=	util.c
 

parser.c
@@ -893,53 +893,37 @@ comma:
 
 /*
  * Parse a declaration making use of preprocessor directives such as the ones
- * provided by queue(3):
- *
- * 	TAILQ_HEAD(x, y);
- * 	TAILQ_HEAD(x, y) z;
- * 	TAILQ_HEAD(x, y) z[S];
- * 	TAILQ_HEAD(x, y) *z;
- * 	TAILQ_HEAD(x, y) z = TAILQ_HEAD_INITIALIZER(z);
- *
- * In addition, detect various macros:
- *
- * 	UMQ_FIXED_EP_DEF() = {
+ * provided by queue(3).
  */
 static int
 parser_exec_decl_cpp(struct parser *pr, struct doc *dc, struct ruler *rl)
 {
 	struct lexer_state s;
 	struct lexer *lx = pr->pr_lx;
-	struct token *end, *ident, *tk;
+	struct token *end, *macro, *tk;
 	struct doc *expr = dc;
-	int semi = 1;
 	int iscpp = 0;
 
 	lexer_peek_enter(lx, &s);
 	while (lexer_if_flags(lx, TOKEN_FLAG_QUALIFIER | TOKEN_FLAG_STORAGE,
 	    NULL))
 		continue;
-	if (lexer_if(lx, TOKEN_IDENT, &ident) &&
+	if (lexer_if(lx, TOKEN_IDENT, &macro) &&
 	    lexer_if_pair(lx, TOKEN_LPAREN, TOKEN_RPAREN, &end)) {
-		if (lexer_if(lx, TOKEN_SEMI, NULL)) {
-			iscpp = 1;
-		} else {
-			for (;;) {
-				if (!lexer_if(lx, TOKEN_STAR, &tk))
-					break;
-				end = tk;
-			}
+		struct token *ident;
 
-			if (lexer_if(lx, TOKEN_IDENT, NULL) &&
-			    (lexer_if(lx, TOKEN_LSQUARE, NULL) ||
-			     lexer_if(lx, TOKEN_SEMI, NULL) ||
-			     lexer_if(lx, TOKEN_EQUAL, NULL) ||
-			     lexer_if(lx, TOKEN_COMMA, NULL)))
-				iscpp = 1;
-			else if (lexer_if(lx, TOKEN_EQUAL, NULL) &&
-			    lexer_if(lx, TOKEN_LBRACE, NULL))
-				iscpp = 1;
+		for (;;) {
+			if (!lexer_if(lx, TOKEN_STAR, &tk))
+				break;
+			end = tk;
 		}
+
+		if (lexer_if(lx, TOKEN_SEMI, NULL) ||
+		    lexer_if(lx, TOKEN_EQUAL, NULL))
+			iscpp = 1;
+		else if (lexer_peek_until(lx, TOKEN_IDENT, &ident) &&
+		    token_cmp(macro, ident) == 0)
+			iscpp = 1;
 	}
 	lexer_peek_leave(lx, &s);
 	if (!iscpp) {
@@ -953,7 +937,7 @@ parser_exec_decl_cpp(struct parser *pr, struct doc *dc, struct ruler *rl)
 
 	if (parser_exec_decl_init(pr, dc, NULL, 0))
 		return parser_error(pr);
-	if (semi && lexer_expect(lx, TOKEN_SEMI, &tk))
+	if (lexer_expect(lx, TOKEN_SEMI, &tk))
 		doc_token(tk, expr);
 
 	return parser_ok(pr);

tests/Makefile
@@ -244,13 +244,13 @@ TESTS+=	valid-199.c
 TESTS+=	valid-200.c
 TESTS+=	valid-201.c
 TESTS+=	valid-202.c
+TESTS+=	valid-203.c
 
 TESTS+=	simple-001.c
 TESTS+=	simple-002.c
 TESTS+=	simple-003.c
 
 TESTS+=	bug-001.c
-TESTS+=	bug-002.c
 TESTS+=	bug-003.c
 
 TESTS+=	../buffer.c

tests/valid-203.c
@@ -1,5 +1,5 @@
 /*
- * queue(3) macro confused as declaration.
+ * Bug: queue(3) macro confused as declaration.
  */
 
 int

commit a0e721178755e33f147fadc4f8f35cc1563d977d

fix parser_exec_decl_cpp() regression
Introduced in commit 33ed4b4 ("do not confuse queue(3) macros as declarations").

Makefile
@@ -421,6 +421,8 @@ DISTFILES+=	tests/valid-206.c
 DISTFILES+=	tests/valid-207.c
 DISTFILES+=	tests/valid-208.c
 DISTFILES+=	tests/valid-208.ok
+DISTFILES+=	tests/valid-209.c
+DISTFILES+=	tests/valid-209.ok
 DISTFILES+=	token.h
 DISTFILES+=	util.c
 

parser.c
@@ -929,6 +929,9 @@ parser_exec_decl_cpp(struct parser *pr, struct doc *dc, struct ruler *rl)
 		else if (lexer_peek_until(lx, TOKEN_IDENT, &ident) &&
 		    token_cmp(macro, ident) == 0)
 			iscpp = 1;
+		else if (lexer_if(lx, TOKEN_IDENT, NULL) &&
+		    lexer_if(lx, TOKEN_SEMI, NULL))
+			iscpp = 1;
 	}
 	lexer_peek_leave(lx, &s);
 	if (!iscpp) {

tests/Makefile
@@ -253,6 +253,7 @@ TESTS+=	valid-205.c
 TESTS+=	valid-206.c
 TESTS+=	valid-207.c
 TESTS+=	valid-208.c
+TESTS+=	valid-209.c
 
 TESTS+=	simple-001.c
 TESTS+=	simple-002.c

tests/valid-209.c
@@ -1,0 +1,6 @@
+/*
+ * Bug: parser_exec_decl_cpp() regression.
+ */
+
+SLIST_ENTRY(taskq_thread)
+			 tt_entry;

tests/valid-209.ok
@@ -1,0 +1,1 @@
+SLIST_ENTRY(taskq_thread) tt_entry;