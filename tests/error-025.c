mptre/knfmt

commit 515428c433bb624a54b759f9997d7b2fe53789fe

correct removal of tokens
The prefixes/suffixes should not be removed until the last reference is dropped.
This unmasked another bug in lexer_branch() in which the actual branch must be
unlinked instead of relying on the odd side effect in token_remove().

lexer.c
@@ -112,7 +112,6 @@ static void		 token_branch_move(struct token *, struct token *,
 static struct token	*token_get_branch(struct token *);
 static struct token	*token_find_prefix(const struct token *,
     enum token_type);
-static void		 token_list_free(struct token_list *);
 static void		 token_remove(struct token_list *, struct token *);
 static const char	*strtoken(enum token_type);
 
@@ -185,8 +184,16 @@ token_rele(struct token *tk)
 		return;
 
 	if ((tk->tk_flags & TOKEN_FLAG_DANGLING) == 0) {
-		token_list_free(&tk->tk_prefixes);
-		token_list_free(&tk->tk_suffixes);
+		struct token *fix;
+
+		while ((fix = TAILQ_FIRST(&tk->tk_prefixes)) != NULL) {
+			token_branch_unlink(fix);
+			token_remove(&tk->tk_prefixes, fix);
+		}
+		while ((fix = TAILQ_FIRST(&tk->tk_suffixes)) != NULL) {
+			token_branch_unlink(fix);
+			token_remove(&tk->tk_suffixes, fix);
+		}
 	}
 	if (tk->tk_flags & TOKEN_FLAG_DIRTY)
 		free((void *)tk->tk_str);
@@ -618,8 +625,9 @@ lexer_branch(struct lexer *lx)
 	    token_sprintf(br->tk_token),
 	    token_sprintf(br->tk_branch.br_nx->tk_token));
 
-	rm = br->tk_token;
+	token_branch_unlink(br);
 
+	rm = br->tk_token;
 	for (;;) {
 		struct token *nx;
 
@@ -2316,24 +2324,9 @@ token_find_prefix(const struct token *tk, enum token_type type)
 }
 
 static void
-token_list_free(struct token_list *tl)
-{
-	struct token *tmp;
-
-	while ((tmp = TAILQ_FIRST(tl)) != NULL) {
-		token_branch_unlink(tmp);
-		token_remove(tl, tmp);
-	}
-}
-
-static void
 token_remove(struct token_list *tl, struct token *tk)
 {
 	TAILQ_REMOVE(tl, tk, tk_entry);
-	if ((tk->tk_flags & TOKEN_FLAG_DANGLING) == 0) {
-		token_list_free(&tk->tk_prefixes);
-		token_list_free(&tk->tk_suffixes);
-	}
 	token_rele(tk);
 }
 

commit 8ce616b44b8906820721c2c8478a622fa6b90ca6

unlink branches on removal
Caused by keeping tokens alive until the last reference is dropped, regression
introduced in commit 515428c ("correct removal of tokens").

Makefile
@@ -181,6 +181,7 @@ DISTFILES+=	tests/error-021.c
 DISTFILES+=	tests/error-022.c
 DISTFILES+=	tests/error-023.c
 DISTFILES+=	tests/error-024.c
+DISTFILES+=	tests/error-025.c
 DISTFILES+=	tests/knfmt.sh
 DISTFILES+=	tests/simple-001.c
 DISTFILES+=	tests/simple-001.ok

lexer.c
@@ -756,8 +756,10 @@ lexer_insert_before(struct lexer *UNUSED(lx), struct token *before,
 void
 lexer_remove(struct lexer *lx, struct token *tk, int keepfixes)
 {
+	struct token *fix;
+
 	if (keepfixes) {
-		struct token *fix, *nx, *pv;
+		struct token *nx, *pv;
 
 		nx = TAILQ_NEXT(tk, tk_entry);
 		assert(nx != NULL);
@@ -776,6 +778,9 @@ lexer_remove(struct lexer *lx, struct token *tk, int keepfixes)
 			TAILQ_REMOVE(&tk->tk_suffixes, fix, tk_entry);
 			TAILQ_INSERT_TAIL(&pv->tk_suffixes, fix, tk_entry);
 		}
+	} else {
+		TAILQ_FOREACH(fix, &tk->tk_prefixes, tk_entry)
+			token_branch_unlink(fix);
 	}
 
 	if (TAILQ_INSERTED(tk, tk_stamp)) {

tests/Makefile
@@ -50,6 +50,7 @@ TESTS+=	error-021.c
 TESTS+=	error-022.c
 TESTS+=	error-023.c
 TESTS+=	error-024.c
+TESTS+=	error-025.c
 
 TESTS+=	valid-001.c
 TESTS+=	valid-002.c

tests/error-025.c
@@ -1,0 +1,17 @@
+/*
+ * AFL
+ */
+
+int
+main(void)
+{
+	if (
+#if 0
+#  if 1
+	0
+#  else
+	1
+#  endif
+#endif
+   )
+}