7407e84fcef1b74333f5c528aa098a36cfdfc778
Anton Lindqvist <anton@basename.se>
2022-05-05 07:45:29 +0200

    more bugs

---
+++ tests/bug-005.c
@@ -0,0 +1,12 @@
+/*
+ * Long binary expression, probably missing a soft line.
+ */
+
+int
+main(void)
+{
+	if (0) {
+		error = parser_exec_decl(pr, concat,
+		    PARSER_EXEC_DECL_FLAG_BREAK | PARSER_EXEC_DECL_FLAG_LINE | PARSER_EXEC_DECL_FLAG_ROOT);
+	}
+}
