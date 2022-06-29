mptre/knfmt

commit cfc47bdeb6ef40ea4c48dbee3b757c38d7244992

copy file path

diff.c
@@ -36,15 +36,16 @@ files_free(struct file_list *files)
 }
 
 struct file *
-file_alloc(char *path, unsigned int flags)
+file_alloc(const char *path)
 {
 	struct file *fe;
 
 	fe = calloc(1, sizeof(*fe));
 	if (fe == NULL)
 		err(1, NULL);
-	fe->fe_path = path;
-	fe->fe_flags = flags;
+	fe->fe_path = strdup(path);
+	if (fe->fe_path == NULL)
+		err(1, NULL);
 	TAILQ_INIT(&fe->fe_diff.di_chunks);
 	return fe;
 }
@@ -61,9 +62,7 @@ file_free(struct file *fe)
 		TAILQ_REMOVE(&fe->fe_diff.di_chunks, du, du_entry);
 		free(du);
 	}
-
-	if (fe->fe_flags & FILE_FLAG_FREE)
-		free(fe->fe_path);
+	free(fe->fe_path);
 	free(fe);
 }
 
@@ -119,7 +118,7 @@ diff_parse(struct file_list *files, const struct config *cf)
 		int el, sl;
 
 		if (matchpath(buf, &path)) {
-			fe = file_alloc(path, FILE_FLAG_FREE);
+			fe = file_alloc(path);
 			TAILQ_INSERT_TAIL(files, fe, fe_entry);
 
 			buf = skipline(buf);

extern.h
@@ -107,8 +107,6 @@ int	diff_covers(const struct diff *, unsigned int);
 struct file {
 	struct diff		 fe_diff;
 	char			*fe_path;
-	unsigned int		 fe_flags;
-#define FILE_FLAG_FREE		0x00000001u	/* fe_path must be freed */
 
 	TAILQ_ENTRY(file)	 fe_entry;
 };
@@ -117,7 +115,7 @@ TAILQ_HEAD(file_list, file);
 
 void	files_free(struct file_list *);
 
-struct file	*file_alloc(char *, unsigned int);
+struct file	*file_alloc(const char *);
 void		 file_free(struct file *);
 
 /*

knfmt.c
@@ -122,13 +122,13 @@ filelist(int argc, char **argv, struct file_list *files,
 		return diff_parse(files, cf);
 
 	if (argc == 0) {
-		fe = file_alloc("/dev/stdin", 0);
+		fe = file_alloc("/dev/stdin");
 		TAILQ_INSERT_TAIL(files, fe, fe_entry);
 	} else {
 		int i;
 
 		for (i = 0; i < argc; i++) {
-			fe = file_alloc(argv[i], 0);
+			fe = file_alloc(argv[i]);
 			TAILQ_INSERT_TAIL(files, fe, fe_entry);
 		}
 	}

t.c
@@ -372,7 +372,7 @@ parser_stub_create(struct parser_stub *ps, const char *src)
 		errc(1, ENAMETOOLONG, "%s", __func__);
 
 	error_init(&ps->ps_er, &cf);
-	ps->ps_fe = file_alloc(ps->ps_path, 0);
+	ps->ps_fe = file_alloc(ps->ps_path);
 	ps->ps_lx = lexer_alloc(ps->ps_fe, &ps->ps_er, &cf);
 	ps->ps_pr = parser_alloc(ps->ps_path, ps->ps_lx, &ps->ps_er, &cf);
 }

commit 03ccd7e44d6e73204e80264ad365ee1aab38c438

plug memory leak
Regression introduced in commit cfc47bd ("copy file path").

diff.c
@@ -11,7 +11,7 @@
 
 static void	diff_end(struct diff *, unsigned int);
 
-static int	matchpath(char *, char **);
+static int	matchpath(char *, char *, size_t);
 static int	matchchunk(char *, int *, int *);
 static int	matchline(const char *, int, struct file *);
 
@@ -114,10 +114,10 @@ diff_parse(struct file_list *files, const struct config *cf)
 	buf = bf->bf_ptr;
 
 	for (;;) {
-		char *path;
+		char path[PATH_MAX];
 		int el, sl;
 
-		if (matchpath(buf, &path)) {
+		if (matchpath(buf, path, sizeof(path))) {
 			fe = file_alloc(path);
 			TAILQ_INSERT_TAIL(files, fe, fe_entry);
 
@@ -193,11 +193,12 @@ diff_end(struct diff *di, unsigned int lno)
 }
 
 static int
-matchpath(char *str, char **path)
+matchpath(char *str, char *path, size_t pathsiz)
 {
 	regmatch_t rm[2];
 	const char *buf;
 	size_t len;
+	int n;
 
 	if (xregexec(&repath, str, rm, 2))
 		return 0;
@@ -209,9 +210,9 @@ matchpath(char *str, char **path)
 		buf += 2;
 		len -= 2;
 	}
-	*path = strndup(buf, len);
-	if (*path == NULL)
-		err(1, NULL);
+	n = snprintf(path, pathsiz, "%.*s", (int)len, buf);
+	if (n < 0 || (size_t)n >= pathsiz)
+		errx(1, "%.*s: path too long", (int)len, buf);
 	return 1;
 }
 