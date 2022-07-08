mptre/knfmt

commit 0f32327a50dc953174b7d7259f77e689f51f18d0

always pass a ruler to parser_exec_func_proto()

parser.c
@@ -99,6 +99,8 @@ static int	parser_exec_expr(struct parser *, struct doc *, struct doc **,
 static int	parser_exec_func_decl(struct parser *, struct doc *,
     struct ruler *, const struct token *);
 static int	parser_exec_func_impl(struct parser *, struct doc *);
+static int	parser_exec_func_impl1(struct parser *, struct doc *,
+    struct ruler *, const struct token *);
 static int	parser_exec_func_proto(struct parser *,
     struct parser_exec_func_proto_arg *);
 static int	parser_exec_func_arg(struct parser *, struct doc *,
@@ -1072,10 +1074,29 @@ parser_exec_func_decl(struct parser *pr, struct doc *dc, struct ruler *rl,
 static int
 parser_exec_func_impl(struct parser *pr, struct doc *dc)
 {
+	struct ruler rl;
+	struct token *type;
+	int error;
+
+	if (parser_peek_func(pr, &type) != PARSER_PEEK_FUNCIMPL)
+		return parser_none(pr);
+
+	ruler_init(&rl, 0);
+	error = parser_exec_func_impl1(pr, dc, &rl, type);
+	if (error & GOOD)
+		ruler_exec(&rl);
+	ruler_free(&rl);
+	return error;
+}
+
+static int
+parser_exec_func_impl1(struct parser *pr, struct doc *dc, struct ruler *rl,
+    const struct token *type)
+{
 	struct parser_exec_func_proto_arg pf = {
 		.pf_dc		= dc,
-		.pf_rl		= NULL,
-		.pf_type	= NULL,
+		.pf_rl		= rl,
+		.pf_type	= type,
 		.pf_line	= DOC_HARDLINE,
 	};
 	struct parser_exec_stmt_block_arg ps = {
@@ -1085,11 +1106,6 @@ parser_exec_func_impl(struct parser *pr, struct doc *dc)
 		.ps_rbrace	= NULL,
 	};
 	struct lexer *lx = pr->pr_lx;
-	struct token *type;
-
-	if (parser_peek_func(pr, &type) != PARSER_PEEK_FUNCIMPL)
-		return parser_none(pr);
-	pf.pf_type = type;
 
 	if (parser_exec_func_proto(pr, &pf) & (FAIL | NONE))
 		return parser_fail(pr);

commit 6063561e9d6915c89c0a5302a2f4101270670a6f

fix whitespace not stripped from function return type regression
Regression introduced in commit 0f32327 ("always pass a ruler to
parser_exec_func_proto()").

Makefile
@@ -511,6 +511,8 @@ DISTFILES+=	tests/valid-234.c
 DISTFILES+=	tests/valid-234.ok
 DISTFILES+=	tests/valid-235.c
 DISTFILES+=	tests/valid-235.ok
+DISTFILES+=	tests/valid-236.c
+DISTFILES+=	tests/valid-236.ok
 DISTFILES+=	token.h
 DISTFILES+=	util.c
 

extern.h
@@ -465,7 +465,7 @@ struct ruler {
 	} rl_columns;
 
 	struct ruler_indent_list	*rl_indent;
-	size_t				 rl_len;
+	int				 rl_len;
 };
 
 struct ruler_column {
@@ -480,7 +480,7 @@ struct ruler_column {
 	size_t	rc_ntabs;
 };
 
-void	ruler_init(struct ruler *, unsigned int);
+void	ruler_init(struct ruler *, int);
 void	ruler_free(struct ruler *);
 void	ruler_exec(struct ruler *);
 

parser.c
@@ -1086,7 +1086,7 @@ parser_exec_func_impl(struct parser *pr, struct doc *dc)
 	if (parser_peek_func(pr, &type) != PARSER_PEEK_FUNCIMPL)
 		return parser_none(pr);
 
-	ruler_init(&rl, 0);
+	ruler_init(&rl, -1);
 	error = parser_exec_func_impl1(pr, dc, &rl, type);
 	if (error & GOOD)
 		ruler_exec(&rl);

ruler.c
@@ -28,8 +28,17 @@ static void	ruler_reset_indent(struct ruler *);
 static int		minimize(const struct ruler_column *);
 static unsigned int	tabalign(unsigned int);
 
+/*
+ * Initialize ruler, the given length is interpreted as follows:
+ *
+ *     >0    Maximum alignment, the aligment for all columns will be less or
+ *           equal to this length.
+ *      0    Use the alignment necessary to align all columns.
+ *     <0    Fixed alignment, unconditionally align all columns to this length
+ *           with its sign inverted.
+ */
 void
-ruler_init(struct ruler *rl, unsigned int len)
+ruler_init(struct ruler *rl, int len)
 {
 	memset(rl, 0, sizeof(*rl));
 	rl->rl_len = len;
@@ -155,7 +164,7 @@ ruler_exec(struct ruler *rl)
 {
 	struct ruler_indent *ri;
 	size_t i, j;
-	unsigned int fixedlen = rl->rl_len;
+	int fixedlen = rl->rl_len;
 	unsigned int maxlen;
 
 	for (i = 0; i < rl->rl_columns.b_len; i++) {
@@ -164,7 +173,7 @@ ruler_exec(struct ruler *rl)
 		if (rc->rc_ntabs == 0 && fixedlen == 0)
 			continue;
 
-		if (fixedlen > 0) {
+		if (fixedlen != 0) {
 			maxlen = fixedlen;
 		} else {
 			maxlen = rc->rc_len;
@@ -178,6 +187,10 @@ ruler_exec(struct ruler *rl)
 			struct ruler_datum *rd = &rc->rc_datums.b_ptr[j];
 			unsigned int indent;
 
+			if (fixedlen < 0) {
+				doc_set_indent(rd->rd_dc, -fixedlen);
+				continue;
+			}
 			if (rd->rd_len > maxlen) {
 				assert(fixedlen > 0);
 				doc_set_indent(rd->rd_dc, 0);

tests/Makefile
@@ -300,6 +300,7 @@ TESTS+=	valid-232.c
 TESTS+=	valid-233.c
 TESTS+=	valid-234.c
 TESTS+=	valid-235.c
+TESTS+=	valid-236.c
 
 TESTS+=	simple-001.c
 TESTS+=	simple-002.c

tests/valid-236.c
@@ -1,0 +1,8 @@
+/*
+ * Trim whitespace in function return type.
+ */
+
+static const char	*
+skipws(const char *str, size_t len)
+{
+}

tests/valid-236.ok
@@ -1,0 +1,4 @@
+static const char *
+skipws(const char *str, size_t len)
+{
+}