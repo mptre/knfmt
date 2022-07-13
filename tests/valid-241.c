mptre/knfmt

commit b56e61e03590c6c239f16816eb6276159124a610

fix detection of openssl type macros such as STACK_OF(...)

Makefile
@@ -82,7 +82,6 @@ DISTFILES+=	t.c
 DISTFILES+=	tests/GNUmakefile
 DISTFILES+=	tests/Makefile
 DISTFILES+=	tests/bug-002.c
-DISTFILES+=	tests/bug-004.c
 DISTFILES+=	tests/bug-005.c
 DISTFILES+=	tests/bug-006.c
 DISTFILES+=	tests/diff-001.c
@@ -184,7 +183,6 @@ DISTFILES+=	tests/error-007.c
 DISTFILES+=	tests/error-008.c
 DISTFILES+=	tests/error-009.c
 DISTFILES+=	tests/error-010.c
-DISTFILES+=	tests/error-011.c
 DISTFILES+=	tests/error-013.c
 DISTFILES+=	tests/error-014.c
 DISTFILES+=	tests/error-015.c
@@ -517,6 +515,7 @@ DISTFILES+=	tests/valid-235.ok
 DISTFILES+=	tests/valid-236.c
 DISTFILES+=	tests/valid-236.ok
 DISTFILES+=	tests/valid-237.c
+DISTFILES+=	tests/valid-238.c
 DISTFILES+=	token.h
 DISTFILES+=	util.c
 

lexer.c
@@ -84,6 +84,7 @@ static void		 lexer_emit_error(struct lexer *, enum token_type,
 
 static int	lexer_peek_if_func_ptr(struct lexer *, struct token **);
 static int	lexer_peek_if_type_ident(struct lexer *lx);
+static int	lexer_peek_if_type_cpp(struct lexer *lx);
 
 static void		 lexer_branch_fold(struct lexer *, struct token *);
 static struct token	*lexer_branch_find(struct token *, int);
@@ -978,6 +979,9 @@ lexer_peek_if_type(struct lexer *lx, struct token **tk, unsigned int flags)
 			if (ntokens == 0)
 				break;
 			peek = 1;
+		} else if (lexer_peek_if_type_cpp(lx)) {
+			lexer_if(lx, TOKEN_IDENT, NULL);
+			lexer_if_pair(lx, TOKEN_LPAREN, TOKEN_RPAREN, &t);
 		} else if (lexer_peek_if(lx, TOKEN_IDENT, NULL)) {
 			struct lexer_state ss;
 			int ident;
@@ -2045,6 +2049,28 @@ lexer_peek_if_type_ident(struct lexer *lx)
 		peek = 1;
 	lexer_peek_leave(lx, &s);
 
+	return peek;
+}
+
+/*
+ * Detect usage of types hidden behind cpp such as STACK_OF(X509).
+ */
+static int
+lexer_peek_if_type_cpp(struct lexer *lx)
+{
+	struct lexer_state s;
+	int peek = 0;
+
+	lexer_peek_enter(lx, &s);
+	if (lexer_if(lx, TOKEN_IDENT, NULL) &&
+	    lexer_if(lx, TOKEN_LPAREN, NULL) &&
+	    lexer_if(lx, TOKEN_IDENT, NULL) &&
+	    lexer_if(lx, TOKEN_RPAREN, NULL) &&
+	    (lexer_if(lx, TOKEN_IDENT, NULL) ||
+	     lexer_if(lx, TOKEN_STAR, NULL) ||
+	     lexer_if(lx, TOKEN_EOF, NULL)))
+		peek = 1;
+	lexer_peek_leave(lx, &s);
 	return peek;
 }
 

t.c
@@ -201,6 +201,12 @@ main(int argc, char *argv[])
 	error |= test_lexer_peek_if_type("void (*)",
 	    "void ( * )");
 	error |= test_lexer_peek_if_type("char (*v)[1]", "char");
+	error |= test_lexer_peek_if_type("STACK_OF(X509_EXTENSION)",
+	    "STACK_OF ( X509_EXTENSION )");
+	error |= test_lexer_peek_if_type("STACK_OF(X509_EXTENSION)*",
+	    "STACK_OF ( X509_EXTENSION ) *");
+	error |= test_lexer_peek_if_type("const STACK_OF(X509_EXTENSION)*",
+	    "const STACK_OF ( X509_EXTENSION ) *");
 
 	error |= test_lexer_peek_if_type_flags(LEXER_TYPE_FLAG_CAST,
 	    "const foo_t)", "const foo_t");

tests/Makefile
@@ -39,7 +39,6 @@ TESTS+=	error-007.c
 TESTS+=	error-008.c
 TESTS+=	error-009.c
 TESTS+=	error-010.c
-TESTS+=	error-011.c
 TESTS+=	error-013.c
 TESTS+=	error-014.c
 TESTS+=	error-015.c
@@ -303,6 +302,7 @@ TESTS+=	valid-234.c
 TESTS+=	valid-235.c
 TESTS+=	valid-236.c
 TESTS+=	valid-237.c
+TESTS+=	valid-238.c
 
 TESTS+=	simple-001.c
 TESTS+=	simple-002.c
@@ -322,7 +322,6 @@ TESTS+=	simple-014.c
 TESTS+=	trace-001.c
 
 TESTS+=	bug-002.c
-TESTS+=	bug-004.c
 TESTS+=	bug-005.c
 TESTS+=	bug-006.c
 

tests/error-011.c
@@ -1,35 +1,0 @@
-/*
- * Confusing return type.
- */
-
-EVP_PKEY *
-load_pubkey(BIO *err, const char *file, int format, int maybe_stdin,
-    const char *pass, const char *key_descrip)
-{
-	if (format == FORMAT_ASN1) {
-		pkey = d2i_PUBKEY_bio(key, NULL);
-	}
-#if !defined(OPENSSL_NO_RC4) && !defined(OPENSSL_NO_RSA)
-	else if (format == FORMAT_NETSCAPE || format == FORMAT_IISSGC)
-		pkey = load_netscape_key(err, key, file, key_descrip, format);
-#endif
-#if !defined(OPENSSL_NO_RSA) && !defined(OPENSSL_NO_DSA)
-	else if (format == FORMAT_MSBLOB)
-		pkey = b2i_PublicKey_bio(key);
-#endif
-	else {
-		BIO_printf(err, "bad input format specified for key file\n");
-		goto end;
-	}
-}
-
-STACK_OF(X509) *
-load_certs(BIO *err, const char *file, int format, const char *pass,
-    const char *desc)
-{
-	STACK_OF(X509) *certs;
-
-	if (!load_certs_crls(err, file, format, pass, desc, &certs, NULL))
-		return NULL;
-	return certs;
-}

tests/valid-238.c
@@ -2,7 +2,7 @@
  * OpenSSL macro return type.
  */
 
-const STACK_OF(X509_EXTENSION)*
+const STACK_OF(X509_EXTENSION) *
 X509_REVOKED_get0_extensions(const X509_REVOKED *x)
 {
 	return x->extensions;

commit 49878fc18e6b46c9ab6d865f0a08edd9bdd9d3d6

fix x macro regression
Introduced in commit b56e61e ("fix detection of openssl type macros such as
STACK_OF(...)").

Makefile
@@ -519,6 +519,7 @@ DISTFILES+=	tests/valid-238.c
 DISTFILES+=	tests/valid-239.c
 DISTFILES+=	tests/valid-239.ok
 DISTFILES+=	tests/valid-240.c
+DISTFILES+=	tests/valid-241.c
 DISTFILES+=	token.h
 DISTFILES+=	util.c
 

lexer.c
@@ -2037,17 +2037,22 @@ static int
 lexer_peek_if_type_cpp(struct lexer *lx)
 {
 	struct lexer_state s;
+	struct token *nx = NULL;
+	struct token *ident;
 	int peek = 0;
 
 	lexer_peek_enter(lx, &s);
-	if (lexer_if(lx, TOKEN_IDENT, NULL) &&
+	if (lexer_if(lx, TOKEN_IDENT, &ident) &&
 	    lexer_if(lx, TOKEN_LPAREN, NULL) &&
 	    lexer_if(lx, TOKEN_IDENT, NULL) &&
 	    lexer_if(lx, TOKEN_RPAREN, NULL) &&
-	    (lexer_if(lx, TOKEN_IDENT, NULL) ||
-	     lexer_if(lx, TOKEN_STAR, NULL) ||
-	     lexer_if(lx, TOKEN_EOF, NULL)))
-		peek = 1;
+	    (lexer_if(lx, TOKEN_IDENT, &nx) ||
+	     lexer_if(lx, TOKEN_STAR, NULL))) {
+		/* Ugly, do not confuse cppx. */
+		peek = nx == NULL ||
+		    ident->tk_len != nx->tk_len ||
+		    strncmp(ident->tk_str, nx->tk_str, nx->tk_len) != 0;
+	}
 	lexer_peek_leave(lx, &s);
 	return peek;
 }

t.c
@@ -200,9 +200,9 @@ main(int argc, char *argv[])
 	error |= test_lexer_peek_if_type("void (*)",
 	    "void ( * )");
 	error |= test_lexer_peek_if_type("char (*v)[1]", "char");
-	error |= test_lexer_peek_if_type("STACK_OF(X509_EXTENSION)",
-	    "STACK_OF ( X509_EXTENSION )");
-	error |= test_lexer_peek_if_type("STACK_OF(X509_EXTENSION)*",
+	error |= test_lexer_peek_if_type("STACK_OF(X509_EXTENSION) x",
+	    "STACK_OF ( X509_EXTENSION ) x");
+	error |= test_lexer_peek_if_type("STACK_OF(X509_EXTENSION) *",
 	    "STACK_OF ( X509_EXTENSION ) *");
 	error |= test_lexer_peek_if_type("const STACK_OF(X509_EXTENSION)*",
 	    "const STACK_OF ( X509_EXTENSION ) *");

tests/Makefile
@@ -305,6 +305,7 @@ TESTS+=	valid-237.c
 TESTS+=	valid-238.c
 TESTS+=	valid-239.c
 TESTS+=	valid-240.c
+TESTS+=	valid-241.c
 
 TESTS+=	simple-001.c
 TESTS+=	simple-002.c

tests/valid-241.c
@@ -1,0 +1,7 @@
+/*
+ * Detection of OpenSSL macro return type regression, confusing such types with
+ * cpp x macros.
+ */
+
+DECLARE_STACK_OF(BY_DIR_HASH)
+DECLARE_STACK_OF(BY_DIR_ENTRY)