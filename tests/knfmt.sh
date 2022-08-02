1d0afff5d8cb24f551bc212b949e486419eaf514
Anton Lindqvist <anton@basename.se>
2022-08-02 08:08:05 +0200

    use testcase helper consistently during testing

--- tests/knfmt.sh
+++ tests/knfmt.sh
@@ -23,18 +23,21 @@ hascomm() {
 	fi
 }
 
-# testcase [-b] [-q] file [-- knfmt-options]
+# testcase [-b] [-e] [-q] file [-- knfmt-options]
 #
 # Run test case.
 testcase() {
-	local _err=0
 	local _bug=0
-	local _quiet=0
+	local _exp=0
 	local _file
+	local _flags="d"
+	local _got=0
+	local _quiet=0
 
 	while [ $# -gt 0 ]; do
 		case "$1" in
 		-b)	_bug=1; _quiet=1;;
+		-e)	_exp=1; _flags="";;
 		-q)	_quiet=1;;
 		*)	break;;
 		esac
@@ -44,7 +47,15 @@ testcase() {
 	[ "${1:-}" = "--" ] && shift
 
 	_ok="${_file%.c}.ok"
-	if [ -e "$_ok" ]; then
+	_patch="${_file%.c}.patch"
+	if [ -e "$_patch" ]; then
+		if ! ${EXEC:-} "${KNFMT}" -v "$@" <"$_patch" 2>&1 |
+			diff -u -L "$_ok" -L "$_file" "$_ok" - >"$_out" 2>&1
+		then
+			cat "$_out" 1>&2
+			return 1
+		fi
+	elif [ -e "$_ok" ]; then
 		_tmp="${_wrkdir}/tmp"
 		commstrip "$_file" >"$_tmp"
 		if ! ${EXEC:-} "${KNFMT}" -v "$@" "$_tmp" 2>&1 |
@@ -54,10 +65,16 @@ testcase() {
 			return 1
 		fi
 	else
-		${EXEC:-} "${KNFMT}" -dv "$@" "$_file" >"$_out" 2>&1 || _err="$?"
-		if [ "$_err" -ne 0 ]; then
-			if [ "$_bug" -eq 1 ] && [ "$_err" -eq 1 ]; then
+		${EXEC:-} "${KNFMT}" "-${_flags}v" "$@" "$_file" \
+			>"$_out" 2>&1 || _got="$?"
+		if [ "$_exp" -ne "$_got" ]; then
+			if [ "$_bug" -eq 1 ] && [ "$_got" -ne 0 ]; then
 				:
+			elif [ "$_exp" -eq 1 ]; then
+				cat "$_out" /dev/stdin 1>&2 <<-EOF
+				${_file}: expected exit 1, got ${_got}
+				EOF
+				return 1
 			else
 				cat "$_out" 1>&2
 				return 1
@@ -106,25 +123,14 @@ case "$1" in
 bug-*)
 	testcase -b "$1" -- -s
 	;;
+diff-simple-*)
+	testcase "$1" -- -Ds
+	;;
 diff-*)
-	_base="${1%.c}"
-	_ok="${_base}.ok"
-	_simple="$([ "${_base#*-simple-}" = "${_base}" ] || echo -s)"
-	if ! ${EXEC:-} "${KNFMT}" -D${_simple:+s}  <"${_base}.patch" 2>&1 | \
-		diff -u -L "$_ok" -L "$1" "$_ok" - >"$_out" 2>&1
-	then
-		cat "$_out" 1>&2
-		exit 1
-	fi
+	testcase "$1" -- -D
 	;;
 error-*)
-	_err=0
-	${EXEC:-} "${KNFMT}" -s "$1" >"$_out" 2>&1 || _err="$?"
-	if [ "$_err" -ne 1 ]; then
-		cat "$_out" 1>&2
-		echo "${1}; expected exit 1, got ${_err}" 1>&2
-		exit 1
-	fi
+	testcase -e -q "$1" -- -s
 	;;
 simple-*|../*)
 	testcase "$1" -- -s
