2a02d8baf39b622b4b3a84474fbabcfde1af5241
Anton Lindqvist <anton@basename.se>
2022-09-05 21:12:48 +0200

    remove unused includes

--- knfmt.c
+++ knfmt.c
@@ -5,10 +5,8 @@
 
 #include <err.h>
 #include <errno.h>
-#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 
 #include "buffer.h"
@@ -18,7 +16,6 @@
 #include "lexer.h"
 #include "options.h"
 #include "parser.h"
-#include "token.h"
 #include "vector.h"
 
 #define _PATH_DIFF	"/usr/bin/diff"
