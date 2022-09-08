b6606a289f474f3b7b63c7ab45769975a8cef873
Anton Lindqvist <anton@basename.se>
2022-09-08 16:13:11 +0200

    improve test runner
    * Always check the exit code.
    * Stop passing the verbose option implicitly.
    * Make error messages stable in the sense of the source code path being
      constant.

--- tests/knfmt.sh
+++ tests/knfmt.sh
@@ -28,6 +28,7 @@ hascomm() {
 # Run test case.
 testcase() {
 	local _bug=0
+	local _diff
 	local _exp=0
 	local _file
 	local _flags="d"
@@ -49,41 +50,44 @@ testcase() {
 	_ok="${_file%.c}.ok"
 	_patch="${_file%.c}.patch"
 	if [ -e "$_patch" ]; then
-		if ! ${EXEC:-} "${KNFMT}" -v "$@" <"$_patch" 2>&1 |
+		if ! ${EXEC:-} "${KNFMT}" "$@" <"$_patch" 2>&1 |
 			diff -u -L "$_ok" -L "$_file" "$_ok" - >"$_out" 2>&1
 		then
 			cat "$_out" 1>&2
 			return 1
 		fi
 	elif [ -e "$_ok" ]; then
-		_tmp="${_wrkdir}/tmp"
+		_tmp="${_wrkdir}/test.c"
+		_diff="${_wrkdir}/diff"
 		commstrip "$_file" >"$_tmp"
-		if ! ${EXEC:-} "${KNFMT}" -v "$@" "$_tmp" 2>&1 |
-			diff -u -L "$_ok" -L "$_file" "$_ok" - >"$_out" 2>&1
-		then
+		(cd "$_wrkdir" && ${EXEC:-} "${KNFMT}" "$@" test.c) \
+			>"$_diff" 2>&1 || _got="$?"
+		if ! diff -u -L "$_ok" -L "$_file" "$_ok" "$_diff" >"$_out" 2>&1; then
 			cat "$_out" 1>&2
 			return 1
 		fi
 	else
-		${EXEC:-} "${KNFMT}" "-${_flags}v" "$@" "$_file" \
+		${EXEC:-} "${KNFMT}" ${_flags:+-${_flags}} "$@" "$_file" \
 			>"$_out" 2>&1 || _got="$?"
-		if [ "$_exp" -ne "$_got" ]; then
-			if [ "$_bug" -eq 1 ] && [ "$_got" -ne 0 ]; then
-				:
-			elif [ "$_exp" -eq 1 ]; then
-				cat "$_out" /dev/stdin 1>&2 <<-EOF
-				${_file}: expected exit 1, got ${_got}
-				EOF
-				return 1
-			else
-				cat "$_out" 1>&2
-				return 1
-			fi
-		elif [ "$_exp" -eq 1 ] && cmp -s - /dev/null <"$_out"; then
-			echo "${_file}: expected error message" 1>&2
+	fi
+
+	if [ "$_exp" -ne "$_got" ]; then
+		if [ "$_bug" -eq 1 ] && [ "$_got" -ne 0 ]; then
+			:
+		elif [ "$_exp" -eq 1 ]; then
+			cat "$_out" /dev/stdin 1>&2 <<-EOF
+			${_file}: expected exit 1, got ${_got}
+			EOF
+			return 1
+		else
+			cat "$_out" 1>&2
 			return 1
 		fi
+	elif [ "$_exp" -eq 1 ] && ! [ -e "$_ok" ] && cmp -s - /dev/null <"$_out"; then
+		echo "${_file}: expected error message" 1>&2
+		return 1
 	fi
+
 	if [ "$_quiet" -eq 0 ] && ! cmp -s /dev/null "$_out"; then
 		cat "$_out" 1>&2
 		return 1
@@ -124,24 +128,24 @@ esac
 
 case "$1" in
 bug-*)
-	testcase -b "$1" -- -s
+	testcase -b "$1" -- -sv
 	;;
 diff-simple-*)
-	testcase "$1" -- -Ds
+	testcase "$1" -- -Dsv
 	;;
 diff-*)
-	testcase "$1" -- -D
+	testcase "$1" -- -Dv
 	;;
 error-*)
 	testcase -e -q "$1" -- -s
 	;;
 simple-*|../*)
-	testcase "$1" -- -s
+	testcase "$1" -- -sv
 	;;
 trace-*)
 	testcase -q "$1" -- -vvv
 	;;
 *)
-	testcase "$1"
+	testcase "$1" -- -v
 	;;
 esac
