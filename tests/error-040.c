mptre/knfmt

commit 85f50ec715afba4ee77bf2e666ddc90213e1abdc

make use of lexer_if() inside lexer_expect()

lexer.c
@@ -920,24 +920,14 @@ __lexer_expect(struct lexer *lx, enum token_type type, struct token **tk,
     const char *fun, int lno)
 {
 	struct token *t = NULL;
-	struct token *pv;
 
-	if (!lexer_back(lx, &pv))
-		pv = NULL;
-
-	if (!lexer_pop(lx, &t))
-		goto err;
-	if (t->tk_type == type) {
-		if (tk != NULL)
-			*tk = t;
-		return 1;
+	if (!lexer_if(lx, type, &t)) {
+		lexer_emit_error(lx, type, t, fun, lno);
+		return 0;
 	}
-
-err:
-	lexer_emit_error(lx, type, t, fun, lno);
-	if (!lexer_is_branch(lx))
-		lx->lx_st.st_tok = pv;
-	return 0;
+	if (tk != NULL)
+		*tk = t;
+	return 1;
 }
 
 void

commit f437737d3818cfa92ebf9c40dfcf4937938d0133

fix error message regression
The current token went missing, make sure this never happens again.
Regression introduced in commit 85f50ec ("make use of lexer_if() inside
lexer_expect()").

Makefile
@@ -263,6 +263,8 @@ DISTFILES+=	tests/error-036.c
 DISTFILES+=	tests/error-037.c
 DISTFILES+=	tests/error-038.c
 DISTFILES+=	tests/error-039.c
+DISTFILES+=	tests/error-040.c
+DISTFILES+=	tests/error-040.ok
 DISTFILES+=	tests/knfmt.sh
 DISTFILES+=	tests/simple-001.c
 DISTFILES+=	tests/simple-001.ok

lexer.c
@@ -921,6 +921,12 @@ __lexer_expect(struct lexer *lx, enum token_type type, struct token **tk,
 	struct token *t = NULL;
 
 	if (!lexer_if(lx, type, &t)) {
+		/*
+		 * Since lexer_if() will not give us a token back in case of
+		 * failure, try to peek at the next one to provide meaningful
+		 * errors.
+		 */
+		lexer_peek(lx, &t);
 		lexer_emit_error(lx, type, t, fun, lno);
 		return 0;
 	}

tests/Makefile
@@ -72,6 +72,7 @@ TESTS+=	error-036.c
 TESTS+=	error-037.c
 TESTS+=	error-038.c
 TESTS+=	error-039.c
+TESTS+=	error-040.c
 
 TESTS+=	valid-001.c
 TESTS+=	valid-002.c

tests/error-040.c
@@ -1,0 +1,9 @@
+/*
+ * Invalid for loop.
+ */
+
+int
+main(void)
+{
+	for ()
+}

tests/error-040.ok
@@ -1,0 +1,1 @@
+test.c: expected type SEMI got RPAREN<4:7>(")")