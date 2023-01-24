154255ce9b33dae9aa7f6ec8b73c9b3e035abaa4
Anton Lindqvist <anton@basename.se>
2023-01-24 07:28:11 +0100

    fix AlignOperands bug related to binary exprs

---
+++ tests/style-AlignOperands-012.c
@@ -0,0 +1,14 @@
+/*
+ * AlignOperands: Align
+ * AlignAfterOpenBracket: Align
+ * BreakBeforeBinaryOperators: NonAssignment
+ */
+
+int
+main(void)
+{
+	uint32 x = 1;
+	vvvvvvvvvvvvvvvvvvvvvvv(aaa,
+	    bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb << 4,
+	    4, cccccccccc);
+}
