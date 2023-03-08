5b8615aeaabd003f2b5ebb5dbce3128000236ab3
Anton Lindqvist <anton@basename.se>
2023-03-08 19:26:46 +0100

    tests: exercise file not found

---
+++ tests/enoent.sh
@@ -0,0 +1,12 @@
+# No such file or directory.
+
+set -e
+
+_wrkdir="$(mktemp -dt knfmt.XXXXXX)"
+trap 'rm -r $_wrkdir' EXIT
+_out="${_wrkdir}/out"
+
+(cd "$_wrkdir" && ${EXEC:-} "$KNFMT" enoent) >"$_out" 2>&1 && exit 1
+diff -u "$_out" - <<EOF
+knfmt: enoent: No such file or directory
+EOF
