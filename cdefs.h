662d5bb3309c7155e5247ff7862e50377e6f68f9
Anton Lindqvist <anton@basename.se>
2023-04-21 07:17:40 +0200

    cope with attribute fallthrough not being supported by older compilers

--- cdefs.h
+++ cdefs.h
@@ -1,10 +1,4 @@
 #define UNUSED(x)	_##x __attribute__((__unused__))
 #define MAYBE_UNUSED(x)	x __attribute__((__unused__))
 
-#if __has_attribute(__fallthrough__)
-#define FALLTHROUGH __attribute__((__fallthrough__))
-#else
-#define FALLTHROUGH do {} while (0)  /* FALLTHROUGH */
-#endif
-
 #define UNLIKELY(x)	__builtin_expect((x), 0)
