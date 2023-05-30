mptre/knfmt

commit f7818e1a96a854381b1468bb0cab96377d587c7f

only need to call simple_leave() when enabled

expr.c
@@ -924,6 +924,7 @@ expr_doc_parens(struct expr *ex, struct expr_state *es, struct doc *dc)
 	if (simple_enter(es->es_ea.si, SIMPLE_EXPR_NOPARENS, &arg, &simple)) {
 		if (ex->ex_lhs != NULL)
 			dc = expr_doc(ex->ex_lhs, es, dc);
+		simple_leave(es->es_ea.si, SIMPLE_EXPR_NOPARENS, simple);
 	} else {
 		struct token *pv;
 
@@ -942,7 +943,6 @@ expr_doc_parens(struct expr *ex, struct expr_state *es, struct doc *dc)
 		if (ex->ex_tokens[1] != NULL)
 			doc_token(ex->ex_tokens[1], dc);	/* ) */
 	}
-	simple_leave(es->es_ea.si, SIMPLE_EXPR_NOPARENS, simple);
 
 	return dc;
 }

commit 8ef007d5150f03522854abd9a779037dbbf63987

fix simple mode not reset regression
Introduced in commit f7818e1 ("only need to call simple_leave() when
enabled").

Makefile
@@ -247,6 +247,7 @@ SHLINT+=	tests/diff.sh
 SHLINT+=	tests/enoent.sh
 SHLINT+=	tests/git.sh
 SHLINT+=	tests/knfmt.sh
+SHLINT+=	tests/simple.sh
 
 DISTFILES+=	CHANGELOG.md
 DISTFILES+=	GNUmakefile
@@ -583,6 +584,7 @@ DISTFILES+=	tests/simple-037.c
 DISTFILES+=	tests/simple-037.ok
 DISTFILES+=	tests/simple-038.c
 DISTFILES+=	tests/simple-038.ok
+DISTFILES+=	tests/simple.sh
 DISTFILES+=	tests/style-AlignAfterOpenBracket-001.c
 DISTFILES+=	tests/style-AlignAfterOpenBracket-002.c
 DISTFILES+=	tests/style-AlignAfterOpenBracket-003.c

expr.c
@@ -924,7 +924,6 @@ expr_doc_parens(struct expr *ex, struct expr_state *es, struct doc *dc)
 	    &simple)) {
 		if (ex->ex_lhs != NULL)
 			dc = expr_doc(ex->ex_lhs, es, dc);
-		simple_leave(es->es_ea.si, SIMPLE_EXPR_NOPARENS, simple);
 	} else {
 		struct token *pv;
 
@@ -943,6 +942,7 @@ expr_doc_parens(struct expr *ex, struct expr_state *es, struct doc *dc)
 		if (ex->ex_tokens[1] != NULL)
 			doc_token(ex->ex_tokens[1], dc);	/* ) */
 	}
+	simple_leave(es->es_ea.si, SIMPLE_EXPR_NOPARENS, simple);
 
 	return dc;
 }

knfmt.c
@@ -124,6 +124,7 @@ main(int argc, char *argv[])
 			error_flush(fe->fe_error, 1);
 		}
 		file_close(fe);
+		simple_sanity_check(si);
 	}
 
 out:

simple.c
@@ -2,10 +2,12 @@
 
 #include "config.h"
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "alloc.h"
+#include "cdefs.h"
 #include "options.h"
 
 struct simple {
@@ -82,6 +84,23 @@ is_simple_enabled(const struct simple *si, enum simple_pass pass)
 {
 	return (si->enable || (si->passes[pass].flags & SIMPLE_FORCE)) &&
 	    si->passes[pass].state == SIMPLE_STATE_ENABLE;
+}
+
+void
+simple_sanity_check(const struct simple *MAYBE_UNUSED(si))
+{
+#ifndef NDEBUG
+	int i;
+
+	for (i = 0; i < SIMPLE_LAST; i++) {
+		if (si->passes[i].state == SIMPLE_STATE_DISABLE)
+			continue;
+
+		fprintf(stderr, "%s: pass %d invalid state: want %d, got %d\n",
+		    __func__, i, SIMPLE_STATE_DISABLE, si->passes[i].state);
+		__builtin_trap();
+	}
+#endif
 }
 
 int

simple.h
@@ -30,5 +30,7 @@ int	simple_enter(struct simple *, enum simple_pass, unsigned int, int *);
 void	simple_leave(struct simple *, enum simple_pass, int);
 int	is_simple_enabled(const struct simple *, enum simple_pass);
 
+void	simple_sanity_check(const struct simple *);
+
 int	simple_disable(struct simple *);
 void	simple_enable(struct simple *, int);

tests/Makefile
@@ -666,3 +666,4 @@ all: ${TESTS:.h=.hfake}
 	sh ${.CURDIR}/diff.sh
 	sh ${.CURDIR}/enoent.sh
 	sh ${.CURDIR}/git.sh
+	sh ${.CURDIR}/simple.sh

tests/simple.sh
@@ -1,0 +1,33 @@
+# Simple mode not reset regression.
+
+set -e
+
+_wrkdir="$(mktemp -dt knfmt.XXXXXX)"
+trap 'rm -r $_wrkdir' EXIT
+cd "$_wrkdir"
+
+cat <<'EOF' >a.c
+int
+main(int argc, char *argv[])
+{
+	while ((0))
+		continue;
+	return 0;
+}
+EOF
+
+cat <<'EOF' >b.c
+int
+main(void)
+{
+	if (0)
+		return 0;
+	else {
+		int x = 0;
+
+		return x;
+	}
+}
+EOF
+
+! ${EXEC:-} "$KNFMT" -ds a.c b.c >/dev/null