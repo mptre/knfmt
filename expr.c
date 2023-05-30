f7818e1a96a854381b1468bb0cab96377d587c7f
Anton Lindqvist <anton@basename.se>
2023-05-30 06:51:14 +0200

    only need to call simple_leave() when enabled

--- expr.c
+++ expr.c
@@ -924,6 +924,7 @@ expr_doc_parens(struct expr *ex, struct expr_state *es, struct doc *dc)
 	if (simple_enter(es->es_ea.si, SIMPLE_EXPR_NOPARENS, &arg, &simple)) {
 		if (ex->ex_lhs != NULL)
 			dc = expr_doc(ex->ex_lhs, es, dc);
+		simple_leave(es->es_ea.si, SIMPLE_EXPR_NOPARENS, simple);
 	} else {
 		struct token *pv;
 
@@ -942,7 +943,6 @@ expr_doc_parens(struct expr *ex, struct expr_state *es, struct doc *dc)
 		if (ex->ex_tokens[1] != NULL)
 			doc_token(ex->ex_tokens[1], dc);	/* ) */
 	}
-	simple_leave(es->es_ea.si, SIMPLE_EXPR_NOPARENS, simple);
 
 	return dc;
 }
