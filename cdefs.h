79724e5e8446fadfcd96147c7104d91fa20abe7b
Anton Lindqvist <anton@basename.se>
2023-06-08 21:17:13 +0200

    lowercase likely macros
    Matches what everyone else already does.

--- cdefs.h
+++ cdefs.h
@@ -1,4 +1,5 @@
 #define UNUSED(x)	_##x __attribute__((__unused__))
 #define MAYBE_UNUSED(x)	x __attribute__((__unused__))
 
-#define UNLIKELY(x)	__builtin_expect((x), 0)
+#define likely(x)	__builtin_expect((x), 1)
+#define unlikely(x)	__builtin_expect((x), 0)
