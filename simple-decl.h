2b49d18d64b8246678dfdb0584cdbf655c4e7b00
Anton Lindqvist <anton@basename.se>
2023-06-14 21:03:31 +0200

    separate prototypes in simple-decl.h

--- simple-decl.h
+++ simple-decl.h
@@ -6,8 +6,7 @@ struct simple_decl	*simple_decl_enter(struct lexer *,
     const struct options *);
 void			 simple_decl_leave(struct simple_decl *);
 void			 simple_decl_free(struct simple_decl *);
-void			 simple_decl_type(struct simple_decl *, struct token *,
-    struct token *);
-void			 simple_decl_semi(struct simple_decl *, struct token *);
-void			 simple_decl_comma(struct simple_decl *,
-    struct token *);
+
+void	simple_decl_type(struct simple_decl *, struct token *, struct token *);
+void	simple_decl_semi(struct simple_decl *, struct token *);
+void	simple_decl_comma(struct simple_decl *, struct token *);
