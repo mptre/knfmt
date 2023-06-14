234b0e6a0d5ecb56a4a06112fa083beccd9194ef
Anton Lindqvist <anton@basename.se>
2023-06-14 21:05:05 +0200

    dog fooding

--- parser-stmt-asm.h
+++ parser-stmt-asm.h
@@ -2,4 +2,4 @@ struct doc;
 struct parser;
 
 int	parser_asm(struct parser *, struct doc *);
-int	parser_stmt_asm(struct parser *pr, struct doc *);
+int	parser_stmt_asm(struct parser *, struct doc *);
