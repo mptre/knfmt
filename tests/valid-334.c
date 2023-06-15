mptre/knfmt

commit d2c2977448111ddf20bd47e73450e40bbe388159

account for alignment in doc_fits()

Makefile
@@ -1151,6 +1151,8 @@ DISTFILES+=	tests/valid-328.ok
 DISTFILES+=	tests/valid-329.c
 DISTFILES+=	tests/valid-330.c
 DISTFILES+=	tests/valid-331.c
+DISTFILES+=	tests/valid-332.c
+DISTFILES+=	tests/valid-332.ok
 DISTFILES+=	token-defs.h
 DISTFILES+=	token.c
 DISTFILES+=	token.h

tests/Makefile
@@ -426,6 +426,7 @@ TESTS+=	valid-328.c
 TESTS+=	valid-329.c
 TESTS+=	valid-330.c
 TESTS+=	valid-331.c
+TESTS+=	valid-332.c
 
 TESTS+=	simple-001.c
 TESTS+=	simple-002.c

doc.c
@@ -267,7 +267,7 @@ static void		doc_trim_lines(const struct doc *, struct doc_state *);
 static int		doc_is_mute(const struct doc_state *);
 static int		doc_parens_align(const struct doc_state *);
 static int		doc_has_list(const struct doc *);
-static void		doc_column(struct doc_state *, const char *, size_t);
+static unsigned int	doc_column(struct doc_state *, const char *, size_t);
 static int		doc_max1(const struct doc *, struct doc_state *,
     void *);
 
@@ -1098,6 +1098,22 @@ static int
 doc_fits1(const struct doc *dc, struct doc_state *st, void *UNUSED(arg))
 {
 	switch (dc->dc_type) {
+	case DOC_ALIGN: {
+		unsigned int indent = dc->dc_align.indent;
+		unsigned int spaces = dc->dc_align.spaces;
+
+		if (dc->dc_align.tabalign) {
+			while (indent > 0) {
+				unsigned int n;
+
+				n = doc_column(st, "\t", 1);
+				indent = n < indent ? indent - n : 0;
+			}
+		}
+		st->st_col += indent + spaces;
+		break;
+	}
+
 	case DOC_LITERAL:
 		doc_column(st, dc->dc_str, dc->dc_len);
 		break;
@@ -1126,7 +1142,6 @@ doc_fits1(const struct doc *dc, struct doc_state *st, void *UNUSED(arg))
 	case DOC_GROUP:
 	case DOC_INDENT:
 	case DOC_NOINDENT:
-	case DOC_ALIGN:
 	case DOC_SOFTLINE:
 	case DOC_HARDLINE:
 	case DOC_MUTE:
@@ -1653,12 +1668,15 @@ doc_has_list(const struct doc *dc)
  * Set the column position, intended to be given the same string just added to
  * the document buffer.
  */
-static void
+static unsigned int
 doc_column(struct doc_state *st, const char *str, size_t len)
 {
+	unsigned int oldcol = st->st_col;
+
 	st->st_col = strwidth(str, len, st->st_col);
 	if (st->st_col > style(st->st_st, ColumnLimit))
 		st->st_stats.nexceeds++;
+	return st->st_col - oldcol;
 }
 
 static int

tests/valid-332.c
@@ -1,0 +1,6 @@
+/*
+ * Long declaration, doc_fits() not accounting for alignment regression.
+ */
+
+struct aaaaaaaaaaaaaaaaa	*bbbbbbbbbbbbbbbbbbbbbbb(void);
+void				 ccccccccccccccccccccccc(struct ddddddddddddddddd *);

tests/valid-332.ok
@@ -1,0 +1,3 @@
+struct aaaaaaaaaaaaaaaaa	*bbbbbbbbbbbbbbbbbbbbbbb(void);
+void				 ccccccccccccccccccccccc(
+    struct ddddddddddddddddd *);

commit 3cae0b9e5e8c175dc33eca88c6c8adf4176889f6

disable expr_doc_soft() logic for brace initializers
Even if the line is too long, we probably want to keep it that way and continue
aligning columns.

Regression introduced in commit d2c2977 ("account for alignment in
doc_fits()d2c2977 account for alignment in doc_fits()").

Makefile
@@ -1163,6 +1163,7 @@ DISTFILES+=	tests/valid-331.c
 DISTFILES+=	tests/valid-332.c
 DISTFILES+=	tests/valid-332.ok
 DISTFILES+=	tests/valid-333.c
+DISTFILES+=	tests/valid-334.c
 DISTFILES+=	token-defs.h
 DISTFILES+=	token.c
 DISTFILES+=	token.h

expr.c
@@ -1228,6 +1228,9 @@ expr_doc_soft0(struct expr *ex, struct expr_state *es, struct doc *dc,
 {
 	struct doc *concat, *parent, *softline;
 
+	if (es->es_flags & EXPR_EXEC_NOSOFT)
+		return expr_doc(ex, es, dc);
+
 	dc = doc_alloc0(DOC_CONCAT, doc_alloc0(DOC_GROUP, dc, 0, fun, lno),
 	    0, fun, lno);
 	softline = doc_alloc0(DOC_SOFTLINE, dc, weight, fun, lno);

expr.h
@@ -25,7 +25,8 @@ struct expr_exec_arg {
 #define EXPR_EXEC_NOPARENS		0x00000008u
 /* Detect inline assembly operands. */
 #define EXPR_EXEC_ASM			0x00000010u
-/* was EXPR_EXEC_TYPE			0x00000020u */
+/* Disable expr_doc_soft() logic. */
+#define EXPR_EXEC_NOSOFT		0x00000020u
 
 	struct {
 		/*

parser-braces.c
@@ -174,7 +174,7 @@ parser_braces1(struct parser *pr, struct braces_arg *arg)
 				.dc	= concat,
 				.rl	= arg->rl,
 				.stop	= stop,
-				.flags	= EXPR_EXEC_ALIGN,
+				.flags	= EXPR_EXEC_ALIGN | EXPR_EXEC_NOSOFT,
 			});
 			if (error & HALT)
 				return parser_fail(pr);

tests/Makefile
@@ -428,6 +428,7 @@ TESTS+=	valid-330.c
 TESTS+=	valid-331.c
 TESTS+=	valid-332.c
 TESTS+=	valid-333.c
+TESTS+=	valid-334.c
 
 TESTS+=	simple-001.c
 TESTS+=	simple-002.c

tests/valid-334.c
@@ -1,0 +1,8 @@
+/*
+ * Brace initializers, soft line expression regression.
+ */
+
+static const struct grammar robsd_regress[] = {
+	{ "regress",		LIST,		config_parse_regress,		REQ|REP,	{ NULL } },
+	{ "regress-env",	LIST,		config_parse_regress_env,	REP,		{ NULL } },
+};