57122eaab0f7d445b129600e025eface5e36f478
Anton Lindqvist <anton@basename.se>
2023-06-30 16:15:00 +0200

    use git-archive(1) in make dist target
    No need for DISTFILES.

--- mkls.sh
+++ mkls.sh
@@ -1,42 +1,21 @@
-export LC_ALL=C
-
-mkls "$@" \
-SRCS \
-	!(fuzz-*|knfmt|t).c \
-	$(find libks -type f -name '*.c' -exec basename {} \;) \
-	-- \
-KNFMT \
-	!(compat-*).c !(compat-*|config).h \
-	compat-pledge.c \
-	-- \
-CLANGTIDY \
-	!(config|compat-*|token-defs).[ch] \
-	-- \
-CPPCHECK \
-	!(compat-*).c \
-	-- \
-SHLINT \
-	configure tests/*.sh \
-	-- \
-DISTFILES \
-	*.c !(config).h libks/*.[ch] *.md \
-	GNUmakefile LICENSE Makefile configure knfmt.1 \
-	tests/*.+(c|ok|patch|sh) \
-	tests/GNUmakefile tests/Makefile
+mkls -s "$@" -- \
+SRCS		!(fuzz-*|knfmt|t).c $(cd libks && ls *.c) -- \
+KNFMT		!(compat-*).c compat-pledge.c !(compat-*|config).h -- \
+CLANGTIDY	!(config|compat-*|token-defs).[ch] -- \
+CPPCHECK	!(compat-*).c -- \
+SHLINT		configure tests/*.sh
 
 cd tests
-mkls "$@" \
-TESTS diff-[0-9]*.c -- \
-TESTS diff-simple-[0-9]*.c -- \
-TESTS diff-style-[0-9]*.c -- \
-TESTS error*.c -- \
-TESTS valid*.c -- \
-TESTS simple*.c -- \
-TESTS inplace*.c -- \
-TESTS trace*.c -- \
-TESTS bug*.c -- \
-TESTS style*.c -- \
-TESTS \
-	../!(compat-*).c ../!(compat-*|config).h \
-	../compat-pledge.c \
-	../libks/*.[ch]
+mkls -s "$@" -- \
+TESTS	diff-[0-9]*.c -- \
+TESTS	diff-simple-[0-9]*.c -- \
+TESTS	diff-style-[0-9]*.c -- \
+TESTS	error*.c -- \
+TESTS	valid*.c -- \
+TESTS	simple*.c -- \
+TESTS	inplace*.c -- \
+TESTS	trace*.c -- \
+TESTS	bug*.c -- \
+TESTS	style*.c -- \
+TESTS	../!(compat-*).c ../!(compat-*|config).h \
+	../compat-pledge.c ../libks/*.[ch]
