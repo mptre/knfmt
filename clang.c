mptre/knfmt

commit 1523fd1cea557ba62b152f5aac5868adae96cbc6

recognize more variants of token aliases
Keywords like asm, attribute, inline, restrict and volatile often comes
in many shapes either preceded or succeeded with underscores. Introduce
a generic solution to detection of such tokens. Internally, they will be
treated as aliases to the corresponding keyword without underscores.

clang.c
@@ -57,6 +57,9 @@ static struct token	*clang_read_cpp(struct clang *, struct lexer *);
 static struct token	*clang_keyword(struct lexer *);
 static struct token	*clang_find_keyword(const struct lexer *,
     const struct lexer_state *);
+static struct token	*clang_find_keyword1(const char *, size_t);
+static struct token	*clang_find_alias(const struct lexer *,
+    const struct lexer_state *);
 static struct token	*clang_ellipsis(struct lexer *,
     const struct lexer_state *);
 
@@ -218,9 +221,10 @@ clang_read(struct lexer *lx, void *arg)
 		} while (isalnum(ch) || ch == '_');
 		lexer_ungetc(lx);
 
-		t = clang_find_keyword(lx, &st);
-		if (t != NULL) {
+		if ((t = clang_find_keyword(lx, &st)) != NULL) {
 			tk = lexer_emit(lx, &st, t);
+		} else if ((tk = clang_find_alias(lx, &st)) != NULL) {
+			/* nothing */
 		} else {
 			/* Fallback, treat everything as an identifier. */
 			tk = lexer_emit(lx, &st, &(struct token){
@@ -666,10 +670,18 @@ static struct token *
 clang_find_keyword(const struct lexer *lx, const struct lexer_state *st)
 {
 	const char *key;
-	size_t i, len;
-	unsigned char slot;
+	size_t len;
 
 	key = lexer_buffer_slice(lx, st, &len);
+	return clang_find_keyword1(key, len);
+}
+
+static struct token *
+clang_find_keyword1(const char *key, size_t len)
+{
+	unsigned char slot;
+	size_t i;
+
 	slot = (unsigned char)key[0];
 	if (table_tokens[slot] == NULL)
 		return NULL;
@@ -680,6 +692,53 @@ clang_find_keyword(const struct lexer *lx, const struct lexer_state *st)
 			return tk;
 	}
 	return NULL;
+}
+
+/*
+ * Recognize aliased token which is a keyword preceded or succeeded with
+ * underscores.
+ */
+static struct token *
+clang_find_alias(const struct lexer *lx, const struct lexer_state *st)
+{
+	const struct {
+		struct {
+			const char	*str;
+			size_t		 len;
+		} alias, keyword;
+	} aliases[] = {
+#define ALIAS(a, k)	{ { a, sizeof(a) - 1, }, { k, sizeof(k) - 1 } }
+		ALIAS("asm",		"asm"),
+		ALIAS("attribute",	"__attribute__"),
+		ALIAS("inline",		"inline"),
+		ALIAS("restrict",	"restrict"),
+		ALIAS("volatile",	"volatile"),
+#undef ALIAS
+	};
+	size_t naliases = sizeof(aliases) / sizeof(aliases[0]);
+	size_t i, len;
+	struct token *kw;
+	const char *str;
+
+	str = lexer_buffer_slice(lx, st, &len);
+	for (; len > 0 && str[0] == '_'; len--, str++)
+		continue;
+	for (; len > 0 && str[len - 1] == '_'; len--)
+		continue;
+	for (i = 0; i < naliases; i++) {
+		if (aliases[i].alias.len == len &&
+		    strncmp(aliases[i].alias.str, str, len) == 0)
+			break;
+	}
+	if (i == naliases)
+		return NULL;
+
+	kw = clang_find_keyword1(aliases[i].keyword.str,
+	    aliases[i].keyword.len);
+	return lexer_emit(lx, st, &(struct token){
+	    .tk_type	= kw->tk_type,
+	    .tk_flags	= kw->tk_flags,
+	});
 }
 
 static struct token *

t.c
@@ -257,11 +257,10 @@ main(int argc, char *argv[])
 	test_lexer_read("...", "ELLIPSIS");
 	test_lexer_read(".x", "PERIOD IDENT");
 
-	test_lexer_read("restrict", "RESTRICT");
-	test_lexer_read("__restrict", "RESTRICT");
-
-	test_lexer_read("__volatile", "VOLATILE");
-	test_lexer_read("__volatile__", "VOLATILE");
+	test_lexer_read("__asm", "ASSEMBLY");
+	test_lexer_read("asm__", "ASSEMBLY");
+	test_lexer_read("__asm__", "ASSEMBLY");
+	test_lexer_read("__attribute", "ATTRIBUTE");
 
 	test_lexer_read("asm_inline", "ASSEMBLY");
 	test_lexer_read("asm_volatile_goto", "ASSEMBLY");

token.h
@@ -89,15 +89,9 @@
 	OP(TOKEN_BACKSLASH, 		"\\", TOKEN_FLAG_DISCARD)
 
 #define FOR_TOKEN_ALIASES(OP)						\
-	OP(TOKEN_ASSEMBLY,	"__asm", 0)				\
-	OP(TOKEN_ASSEMBLY,	"__asm__", 0)				\
 	/* linux */							\
 	OP(TOKEN_ASSEMBLY,	"asm_inline", 0)			\
-	OP(TOKEN_ASSEMBLY,	"asm_volatile_goto", 0)			\
-	OP(TOKEN_ATTRIBUTE,	"__attribute", 0)			\
-	OP(TOKEN_RESTRICT,	"__restrict", TOKEN_FLAG_QUALIFIER)	\
-	OP(TOKEN_VOLATILE,	"__volatile", TOKEN_FLAG_QUALIFIER)	\
-	OP(TOKEN_VOLATILE,	"__volatile__", TOKEN_FLAG_QUALIFIER)
+	OP(TOKEN_ASSEMBLY,	"asm_volatile_goto", 0)
 
 #define FOR_TOKEN_SENTINELS(OP)						\
 	OP(TOKEN_COMMENT,	"", 0)					\

commit 68fd74eac23973843d40189743253d303645cb18

fix bug in token with underscore(s) alias detection
Regression introduced in 1523fd1 ("recognize more variants of token aliases").

clang.c
@@ -722,12 +722,15 @@ clang_find_alias(const struct lexer *lx, const struct lexer_state *st)
 	size_t i, len;
 	struct token *kw;
 	const char *str;
+	int nunderscores = 0;
 
 	str = lexer_buffer_slice(lx, st, &len);
 	for (; len > 0 && str[0] == '_'; len--, str++)
-		continue;
+		nunderscores++;
 	for (; len > 0 && str[len - 1] == '_'; len--)
-		continue;
+		nunderscores++;
+	if (nunderscores == 0)
+		return NULL;
 	for (i = 0; i < naliases; i++) {
 		if (aliases[i].alias.len == len &&
 		    strncmp(aliases[i].alias.str, str, len) == 0)

t.c
@@ -308,6 +308,7 @@ main(int argc, char *argv[])
 	test_lexer_read("asm__", "ASSEMBLY");
 	test_lexer_read("__asm__", "ASSEMBLY");
 	test_lexer_read("__attribute", "ATTRIBUTE");
+	test_lexer_read("attribute", "IDENT");
 
 	test_lexer_read("asm_inline", "ASSEMBLY");
 	test_lexer_read("asm_volatile_goto", "ASSEMBLY");