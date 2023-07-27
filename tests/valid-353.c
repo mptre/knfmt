mptre/knfmt

commit d9b1a48bb3322d13a752b49f0e02ce1965173b5b

add bool tokens

token.h
@@ -6,6 +6,7 @@
 	/* keywords */							\
 	OP(TOKEN_ASSEMBLY,	"asm", 0)				\
 	OP(TOKEN_ATTRIBUTE,	"__attribute__", 0)			\
+	OP(TOKEN_BOOL,		"_Bool", TOKEN_FLAG_TYPE)		\
 	OP(TOKEN_BREAK,		"break", 0)				\
 	OP(TOKEN_CASE,		"case", 0)				\
 	OP(TOKEN_CHAR,		"char", TOKEN_FLAG_TYPE)		\
@@ -91,7 +92,9 @@
 #define FOR_TOKEN_ALIASES(OP)						\
 	/* linux */							\
 	OP(TOKEN_ASSEMBLY,	"asm_inline", 0)			\
-	OP(TOKEN_ASSEMBLY,	"asm_volatile_goto", 0)
+	OP(TOKEN_ASSEMBLY,	"asm_volatile_goto", 0)			\
+	/* stdbool.h */							\
+	OP(TOKEN_BOOL,		"bool", 0)
 
 #define FOR_TOKEN_SENTINELS(OP)						\
 	OP(TOKEN_COMMENT,	"", 0)					\

commit 4da6ae78f8fc5d9ab03038ee4154549de0effb4a

fix regression introduced with adding bool token aliases
These aliases are ambiguous as they are eligible for variable names without
further typedef/cpp knowledge.

Regression introduced in commit d9b1a48 ("add bool tokens").

token.h
@@ -106,9 +106,7 @@
 	OP(TOKEN_UINT64,	"u_int64_t", 0)				\
 	/* Linux */							\
 	OP(TOKEN_ASSEMBLY,	"asm_inline", 0)			\
-	OP(TOKEN_ASSEMBLY,	"asm_volatile_goto", 0)			\
-	/* stdbool.h */							\
-	OP(TOKEN_BOOL,		"bool", 0)
+	OP(TOKEN_ASSEMBLY,	"asm_volatile_goto", 0)
 
 #define FOR_TOKEN_SENTINELS(OP)						\
 	OP(TOKEN_COMMENT,	"", 0)					\

tests/Makefile
@@ -453,6 +453,7 @@ TESTS+=	valid-349.c
 TESTS+=	valid-350.c
 TESTS+=	valid-351.c
 TESTS+=	valid-352.c
+TESTS+=	valid-353.c
 
 TESTS+=	simple-002.c
 TESTS+=	simple-003.c

tests/valid-353.c
@@ -1,0 +1,44 @@
+/*
+ * Bool alias tokens regression.
+ */
+
+int
+xdr_exports(XDR *xdrsp, struct exportslist **exp)
+{
+	struct exportslist *ep;
+	struct grouplist *gp;
+	int bool, grpbool;
+	char *strp;
+
+	*exp = NULL;
+	if (!xdr_bool(xdrsp, &bool))
+		return (0);
+	while (bool) {
+		ep = malloc(sizeof(struct exportslist));
+		if (ep == NULL)
+			return (0);
+		ep->ex_groups = NULL;
+		strp = ep->ex_dirp;
+		if (!xdr_string(xdrsp, &strp, RPCMNT_PATHLEN))
+			return (0);
+		if (!xdr_bool(xdrsp, &grpbool))
+			return (0);
+		while (grpbool) {
+			gp = malloc(sizeof(struct grouplist));
+			if (gp == NULL)
+				return (0);
+			strp = gp->gr_name;
+			if (!xdr_string(xdrsp, &strp, RPCMNT_NAMELEN))
+				return (0);
+			gp->gr_next = ep->ex_groups;
+			ep->ex_groups = gp;
+			if (!xdr_bool(xdrsp, &grpbool))
+				return (0);
+		}
+		ep->ex_next = *exp;
+		*exp = ep;
+		if (!xdr_bool(xdrsp, &bool))
+			return (0);
+	}
+	return (1);
+}