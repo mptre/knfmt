2b0627de4f24d766307616d0cba60ce13fb77748
Anton Lindqvist <anton@basename.se>
2023-07-31 07:46:57 +0200

    fix variable typo

--- parser-expr.c
+++ parser-expr.c
@@ -133,16 +133,16 @@ expr_recover_cast(const struct expr_exec_arg *UNUSED(ea), struct doc *dc,
 	struct lexer_state s;
 	struct parser *pr = arg;
 	struct lexer *lx = pr->pr_lx;
-	struct token *op, *rparens;
+	struct token *op, *rparen;
 	int peek = 0;
 
 	lexer_peek_enter(lx, &s);
 	if (parser_type_peek(pr, &type, PARSER_TYPE_CAST) &&
 	    lexer_seek_after(lx, type.end) &&
-	    lexer_if(lx, TOKEN_RPAREN, &rparens) &&
+	    lexer_if(lx, TOKEN_RPAREN, &rparen) &&
 	    !lexer_if(lx, TOKEN_COMMA, NULL) &&
 	    !(lexer_peek_if_flags(lx, TOKEN_FLAG_BINARY, &op) &&
-	    token_has_spaces(rparens) && token_has_spaces(op)) &&
+	    token_has_spaces(rparen) && token_has_spaces(op)) &&
 	    !(lexer_if(lx, TOKEN_AMP, NULL) &&
 	    lexer_if(lx, TOKEN_TILDE, NULL)) &&
 	    !lexer_if(lx, LEXER_EOF, NULL))
