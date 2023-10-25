81ebfde1093177cb1e256e4e6962394ff7213e38
Anton Lindqvist <anton@basename.se>
2023-10-25 20:20:44 +0200

    fuzz: cope with latest changes

--- fuzz-style.c
+++ fuzz-style.c
@@ -1,18 +1,28 @@
 #include "config.h"
 
+#include "libks/arena.h"
+
 #include "options.h"
 #include "style.h"
 
 int
 main(void)
 {
+	struct arena *eternal, *scratch;
 	struct options op;
 	struct style *st;
 
 	options_init(&op);
 	style_init();
-	st = style_parse("/dev/stdin", &op);
+	eternal = arena_alloc(ARENA_FATAL);
+	scratch = arena_alloc(ARENA_FATAL);
+	arena_scope(eternal, eternal_scope);
+
+	st = style_parse("/dev/stdin", &eternal_scope, scratch, &op);
 	style_free(st);
+
 	style_shutdown();
+	arena_free(scratch);
+	arena_free(eternal);
 	return 0;
 }
