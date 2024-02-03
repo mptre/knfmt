5b103e99608df8903524532c5836a0ee0f65df29
Anton Lindqvist <anton@basename.se>
2024-02-03 09:08:10 +0100

    simple-decl-forward: add cpp branch test cases

---
+++ tests/simple-decl-forward-005.c
@@ -0,0 +1,10 @@
+/*
+ * Sort forward declarations.
+ */
+
+struct d;
+#if 0
+struct c;
+#endif
+struct b;
+struct a;
---
+++ tests/simple-decl-forward-006.c
@@ -0,0 +1,11 @@
+/*
+ * Sort forward declarations.
+ */
+
+struct d;
+#if 0
+struct c;
+#else
+struct b;
+#endif
+struct a;
