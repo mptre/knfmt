9279b6d7c71429b90b829f498b47a61f8f8411a9
Anton Lindqvist <anton@basename.se>
2024-06-30 20:48:21 +0200

    clang: fix NDEBUG compilation

--- clang.c
+++ clang.c
@@ -1362,10 +1362,8 @@ token_move_prefix(struct token *prefix, struct token *src, struct token *dst)
 	if (token_type == TOKEN_CPP_IF ||
 	    token_type == TOKEN_CPP_ELSE ||
 	    token_type == TOKEN_CPP_ENDIF) {
-		struct clang_token *NDEBUG_UNUSED(ct);
-
-		ct = token_priv(prefix, struct clang_token);
-		assert(ct->branch.parent == src);
+		assert(token_priv(prefix, struct clang_token)->branch.parent ==
+		    src);
 		token_branch_parent(prefix, dst);
 		token_branch_parent_update_flags(dst);
 		token_branch_parent_update_flags(src);
