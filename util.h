f2a687cd3d2f8bc39ff38d39b5edf35f3c374d3c
Anton Lindqvist <anton@basename.se>
2024-08-06 07:48:38 +0200

    util: group includes in header

--- util.h
+++ util.h
@@ -3,13 +3,12 @@
 struct arena_scope;
 struct buffer;
 
-unsigned int	colwidth(const char *, size_t, unsigned int, unsigned int *);
-
 const char	*path_slice(const char *, unsigned int, struct arena_scope *);
 
-size_t	strindent_buffer(struct buffer *, size_t, int, size_t);
+unsigned int	colwidth(const char *, size_t, unsigned int, unsigned int *);
+size_t		strwidth(const char *, size_t, size_t);
 
 const char	*strnice(const char *, size_t, struct arena_scope *);
 void		 strnice_buffer(struct buffer *, const char *, size_t);
 
-size_t	strwidth(const char *, size_t, size_t);
+size_t	strindent_buffer(struct buffer *, size_t, int, size_t);
