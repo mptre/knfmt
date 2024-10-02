bbcf4c2172cd5ef84c88eb3c16d2a826f2a902ef
Anton Lindqvist <anton@basename.se>
2024-10-02 09:09:24 +0200

    remove unused stdio.h includes

--- simple-decl.c
+++ simple-decl.c
@@ -4,7 +4,6 @@
 
 #include <assert.h>
 #include <err.h>
-#include <stdio.h>
 #include <string.h>
 
 #include "libks/arena-buffer.h"
--- t.c
+++ t.c
@@ -1,6 +1,5 @@
 #include "config.h"
 
-#include <stdio.h>
 #include <string.h>
 
 #include "libks/arena-buffer.h"
